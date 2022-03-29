96b441acd4c72c71dac1b6fcef883ba839c8bf7f
Saurin Dashadia <5389966+saurindashadia@users.noreply.github.com>
2022-03-29 14:51:48 +0530

    Revert "Docs"

---
+++ includes/class-wp-developer-debug-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://symaticsolutions.com
+ * @since      0.1.0
+ *
+ * @package    Wp_Developer_Debug
+ * @subpackage Wp_Developer_Debug/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      0.1.0
+ * @package    Wp_Developer_Debug
+ * @subpackage Wp_Developer_Debug/includes
+ * @author     Symatic Solutions <info>
+ */
+class Wp_Developer_Debug_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    0.1.0
+	 */
+	public static function activate() {
+
+	}
+
+}
