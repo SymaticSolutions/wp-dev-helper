9a46ef616f58ea40ca7f7f8d5b0190b9822ced1b
Saurin Dashadia <saurindashadia@gmail.com>
2022-03-31 17:52:59 +0530

    Upon plugin activation add new role 'Developer' we will use this user role to show all debug information so that other roles are not affected with debug scripts.

--- includes/class-wp-developer-debug-activator.php
+++ includes/class-wp-developer-debug-activator.php
@@ -23,13 +23,38 @@
 class Wp_Developer_Debug_Activator {
 
 	/**
-	 * Short Description. (use period)
+	 * Activation Script
 	 *
-	 * Long Description.
+	 * Upon plugin activation add new user role 'Developer' and setup capabilities.
 	 *
 	 * @since    0.1.0
 	 */
 	public static function activate() {
+		if(is_multisite()){ // if multisite is enabled we need to set developer capabilities similar to Super Admin.
+			add_role(
+				'developer',
+				'Developer',
+				get_role( 'administrator' )->capabilities // Administrator capabilities.
+				+ array( // Super Admin capabilities.
+					'create_sites' => 1,
+					'delete_sites' => 1,
+					'manage_network' => 1,
+					'manage_sites' => 1,
+					'manage_network_users' => 1,
+					'manage_network_plugins' => 1,
+					'manage_network_themes' => 1,
+					'manage_network_options' => 1,
+					'upgrade_network' => 1,
+					'setup_network' => 1
+				)
+			);
+		}else{
+			add_role(
+				'developer',
+				'Developer',
+				get_role( 'administrator' )->capabilities // Administrator capabilities.
+			);
+		}
 
 	}
 
